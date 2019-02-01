377f32e88a55996974faaa7431699b03305b73c7
Clara <clara.081094@gmail.com>
2019-02-01 12:45:56 -0500

    Gallerry view for recognized faces

--- app.py
+++ app.py
@@ -1,20 +1,32 @@
 
 from flask import Flask, render_template
-
+import os
 # Import the images dictionary
-from models.images import images
+#from models.images import images
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/caras')
 def hello_world():
     # Pass the images dictionary data to the gallery page
-    return render_template('views/gallery.html', images=images)
+
+    lista=[]
+    path="/home/ubuntu/proyectos/microservicePlatanitos/pictures/"
+
+    cameras = os.listdir(path)
+    for camera in cameras:
+        images=os.listdir(path+str(camera)+"/registered/")
+        for face in images:
+            item ={"camera":camera,"face":face}
+            lista.append(item)
+
+    return render_template('views/gallery.html',
+    faces=lista)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8000)
 
 
 
--- models/__init__.py
+++
@@ -1,1 +0,0 @@
-__Author__ = 'El Jesco'
